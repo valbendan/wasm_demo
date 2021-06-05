fcdc2c49097ca03601d92f7c5df19a6b042d4649
valbendan <valbendan@hotmail.com>
2021-06-05 22:33:33 +0800

    add simple demo

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+build:
+	yarn asc demo.ts -b demo.wasm -t demo.wat --optimize
---
+++ demo.ts
@@ -0,0 +1,9 @@
+/**
+ * add demo for wasm
+ * return `x` + `y` + 1 value
+ * @param x
+ * @param y
+ */
+export function add_demo(x: number, y: number): number {
+    return x + y + 1
+}
