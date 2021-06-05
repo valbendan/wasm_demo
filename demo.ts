70e0fac770b58337570b9d52976ebcc57e72a743
valbendan <valbendan@hotmail.com>
2021-06-05 22:50:54 +0800

    import function works :)

--- demo.ts
+++ demo.ts
@@ -1,9 +1,14 @@
 /**
+ * 需要导入的值
+ */
+declare let increment: number;
+
+/**
  * add demo for wasm
  * return `x` + `y` + 1 value
  * @param x
  * @param y
  */
 export function add_demo(x: number, y: number): number {
-    return x + y + 1
+    return x + y + increment
 }
--- main.mjs
+++ main.mjs
@@ -3,9 +3,11 @@ import * as assert from "assert";
 
 const binCode = fs.readFileSync("demo.wasm");
 
-const wasm = await WebAssembly.instantiate(binCode);
+const increment = new WebAssembly.Global({mutable: true, value: 'f64'}, 2);
+
+const wasm = await WebAssembly.instantiate(binCode, {demo: {increment: increment}});
 
 const v = wasm.instance.exports.add_demo(2, 3);
 
 console.log({v: v});
-assert.strictEqual(v, 6);
+assert.strictEqual(v, 7);
